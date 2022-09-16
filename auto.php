b4a66ad78bc9935e83539e3639a9cd23285e11d1
cosmii02 <cosmii02@cosmii02.com>
2022-09-16 12:20:52 +0300

    quick edit to html

---
+++ auto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$tootja = "Audi";
+$varv = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "Minu uus " . $tootja . "<br>";
+echo $x + $y;
+?>
+
+</body>
+</html>
